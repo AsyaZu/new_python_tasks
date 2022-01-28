15cd0a947ae73c7dc775036532233d27776f1fb5
Asya <nastya.Z1998@yandex.ru>
2022-01-28 18:27:08 +0500

    14

--- 14.py
+++ 14.py
@@ -1,4 +1,16 @@
 # Подсчитать сумму цифр в вещественном числе
 
-num = 14.3456
-print(len(str(num)) - 1)
+def sum (n):
+    sum = 0
+    n1 = int(n)
+    n2 = n - n1
+    while n1 > 0:
+        sum = sum + n1 % 10
+        n1 = n // 10
+    while n2 > 0:
+        n2 = n2 * 10
+        sum = sum + int(n2)
+        n2 = n2 - int(n2)
+    return sum
+    
+print(sum(14.23))
