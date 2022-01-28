f78eae354bcad186c751942124ec698cbe7b82b7
Asya <nastya.Z1998@yandex.ru>
2022-01-28 18:26:03 +0500

    15

--- 15.py
+++ 15.py
@@ -1,3 +1,15 @@
 # Написать программу получающую набор произведений чисел от 1 до N.
 # Пример: пусть N = 4, тогда
 # [ 1, 2, 6, 24 ]
+
+def numbers (n):
+    list = []
+    fact = 1
+    i = 1
+    while i <= n:
+        fact *= i
+        list.append(fact)
+        i += 1
+    return list
+
+print(numbers(5)) 
