23a6d2fcd5038a3235583838fe0e51b984310412
Asya <92664686+AsyaZu@users.noreply.github.com>
2022-01-29 12:54:20 +0500

    17

--- 17.py
+++ 17.py
@@ -3,6 +3,11 @@
 # Позиции хранятся в файле file.txt в одной строке одно число
 from random import*
 
+with ('file.txt', 'w') as data:
+    data.write('0\n')
+    data.write('1\n')
+    data.write('2\n')
+
 def array (n):
     list = [randrange(-n, n + 1) for i in range(n)]
     data = open('file.txt', 'r')
