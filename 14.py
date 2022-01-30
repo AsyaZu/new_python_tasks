5d5b08b1731177770a1a82060d0ded9aa8c0bd34
Asya <nastya.Z1998@yandex.ru>
2022-01-30 12:18:37 +0500

    14

--- 14.py
+++ 14.py
@@ -6,11 +6,11 @@ def sum (n):
     n2 = n - n1
     while n1 > 0:
         sum = sum + n1 % 10
-        n1 = n // 10
+        n1 = n1 // 10
     while n2 > 0:
         n2 = n2 * 10
         sum = sum + int(n2)
         n2 = n2 - int(n2)
     return sum
     
-print(sum(14.23))
+print(sum(14.231))
